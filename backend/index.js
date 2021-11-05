d6deb830904e50ef76a6764d846d3b2fc0b005d1
ujangaja <ujangaja@gmail.com>
2021-11-05 15:49:39 +0700

    Connected to Database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import db from './config/Database.js';
 const app = express();
 
+try {
+  await db.authenticate();
+  console.log('Database Connected...');
+} catch (error) {
+  console.error(error);
+}
 app.listen(5000, () => console.log('App listening on port 5000!'));
