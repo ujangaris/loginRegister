510014b35d11739e3ee50ba41f2d9c5570fd4f8b
ujangaja <ujangaja@gmail.com>
2021-11-06 03:53:46 +0700

    Logout

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, Register, Login } from '../controllers/Users.js';
+import { getUsers, Register, Login, Logout } from '../controllers/Users.js';
 import { verifyToken } from '../middleware/VerifyToken.js';
 import { refreshToken } from '../controllers/RefreshToken.js';
 
@@ -9,5 +9,6 @@ router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
+router.delete('/logout', Logout);
 
 export default router;
