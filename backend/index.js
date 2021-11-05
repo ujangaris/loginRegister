c0a7a7dd3e3aa54c2f0966170c1e201d4d81b090
ujangaja <ujangaja@gmail.com>
2021-11-06 04:00:22 +0700

    Cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import db from './config/Database.js';
 // import Users from './models/UserModel.js';// ini dinyalakn jika pertama kali mau buat table user
 import router from './routes/index.js';
@@ -15,6 +16,7 @@ try {
   console.error(error);
 }
 
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' })); //localhost 3000 ini default dari reactJs, karna kita akan menggunakan react js
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
