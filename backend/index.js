e88cbb84151d5feab4607743b4e8b8e675f82fec
ujangaja <ujangaja@gmail.com>
2021-11-05 21:04:35 +0700

    Refresh Token

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import db from './config/Database.js';
 // import Users from './models/UserModel.js';// ini dinyalakn jika pertama kali mau buat table user
 import router from './routes/index.js';
@@ -14,6 +15,7 @@ try {
   console.error(error);
 }
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getUsers, Register, Login } from '../controllers/Users.js';
 import { verifyToken } from '../middleware/VerifyToken.js';
+import { refreshToken } from '../controllers/RefreshToken.js';
 
 const router = express.Router();
 
 router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
+router.get('/token', refreshToken);
 
 export default router;
