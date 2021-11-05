be9f0e363e4df6051e2664039dfbcb1f263f4cc6
ujangaja <ujangaja@gmail.com>
2021-11-05 20:34:59 +0700

    Middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getUsers, Register, Login } from '../controllers/Users.js';
+import { verifyToken } from '../middleware/VerifyToken.js';
 
 const router = express.Router();
 
-router.get('/users', getUsers);
+router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
 
