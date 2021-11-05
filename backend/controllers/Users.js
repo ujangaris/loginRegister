c45efb321f3140714bf67967fa4278aa37ef622f
ujangaja <ujangaja@gmail.com>
2021-11-05 16:25:26 +0700

    Send Request Users

---
+++ backend/controllers/Users.js
@@ -0,0 +1,10 @@
+import Users from '../models/UserModel.js';
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await Users.findAll();
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './config/Database.js';
 // import Users from './models/UserModel.js';// ini dinyalakn jika pertama kali mau buat table user
+import router from './routes/index.js';
 const app = express();
 
 try {
@@ -10,4 +11,8 @@ try {
 } catch (error) {
   console.error(error);
 }
+
+app.use(express.json());
+app.use(router);
+
 app.listen(5000, () => console.log('App listening on port 5000!'));
