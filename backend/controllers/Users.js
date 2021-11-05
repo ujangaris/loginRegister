03a7276fa832dfca98915eb3de6c6ccf0f3cf413
ujangaja <ujangaja@gmail.com>
2021-11-05 18:45:05 +0700

    Create Data User

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,9 +1,30 @@
 import Users from '../models/UserModel.js';
-
+import bcrypt from 'bcrypt';
 export const getUsers = async (req, res) => {
   try {
     const users = await Users.findAll();
     res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Register
+export const Register = async (req, res) => {
+  const { name, email, password, confPassword } = req.body;
+  if (password !== confPassword)
+    return res
+      .status(400)
+      .json({ msg: 'Password dan Confirm Password tidak cocok' });
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  try {
+    await Users.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+    });
+    res.json({ msg: 'Register Berhasil' });
   } catch (error) {
     console.log(error);
   }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getUsers } from '../controllers/Users.js';
+import { getUsers, Register } from '../controllers/Users.js';
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.post('/users', Register);
 
 export default router;
