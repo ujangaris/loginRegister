cc59e493b23f88c70c8c5cd531adbf3b1debd4a5
ujangaja <ujangaja@gmail.com>
2021-11-05 16:08:02 +0700

    Creat table user dengan schema dan Sequelize

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import db from './config/Database.js';
+// import Users from './models/UserModel.js';// ini dinyalakn jika pertama kali mau buat table user
 const app = express();
 
 try {
   await db.authenticate();
   console.log('Database Connected...');
+  //   await Users.sync(); // untuk mengenerate Table User dengan squelize jika kita belum punya table user, dan ini dinyalakn jika pertama kali mau buat table user
 } catch (error) {
   console.error(error);
 }
---
+++ backend/models/UserModel.js
@@ -0,0 +1,27 @@
+import { Sequelize } from 'sequelize';
+import db from '../config/Database.js';
+
+const { DataTypes } = Sequelize;
+
+const Users = db.define(
+  'users',
+  {
+    name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    refresh_token: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    freezTableName: true,
+  }
+);
+
+export default Users;
