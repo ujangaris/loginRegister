65ceca568b1d9b310f30db29f23a9860b73c2765
ujangaja <ujangaja@gmail.com>
2021-11-05 20:41:03 +0700

    filter output yang tampil ketika berhasil login

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -4,7 +4,9 @@ import jwt from 'jsonwebtoken';
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await Users.findAll();
+    const users = await Users.findAll({
+      attributes: ['id', 'name', 'email'],
+    });
     res.json(users);
   } catch (error) {
     console.log(error);
