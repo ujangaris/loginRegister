2b70c3f44f2af29e9e967dd6a878243481af9126
ujangaja <ujangaja@gmail.com>
2021-11-06 18:38:42 +0700

    Dashboard dan Navbar Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
 import Register from './components/Register';
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/dashboard">
+          <Navbar />
+          <Dashboard />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+// rafce
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome Back:</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
