175a7a9ce852ab552f6a66d7fe2b54a01e23c194
ujangaja <ujangaja@gmail.com>
2021-11-07 15:21:05 +0700

    Menampilkan nama user yang login pada halaman dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,29 @@
 // rafce
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwt_decoode from 'jwt-decode';
+import { useHistory } from 'react-router-dom';
 
 const Dashboard = () => {
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/token');
+      setToken(response.data.accessToken);
+      const decode = jwt_decoode(response.data.accessToken);
+      // console.log(decode);
+      setName(decode.name);
+    } catch (error) {}
+  };
   return (
     <div className="container mt-5">
-      <h1>Welcome Back:</h1>
+      <h1>Welcome Back: {name}</h1>
     </div>
   );
 };
