991c5d9eb4488336605a99cdb22e9d17884e128e
ujangaja <ujangaja@gmail.com>
2021-11-07 16:24:30 +0700

    Lifetime token

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,9 +7,12 @@ import { useHistory } from 'react-router-dom';
 const Dashboard = () => {
   const [name, setName] = useState('');
   const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     refreshToken();
+    getUsers();
   }, []);
 
   const refreshToken = async () => {
@@ -19,11 +22,48 @@ const Dashboard = () => {
       const decode = jwt_decoode(response.data.accessToken);
       // console.log(decode);
       setName(decode.name);
-    } catch (error) {}
+      setExpire(decode.exp);
+    } catch (error) {
+      if (error.response) {
+        history.push('/');
+      }
+    }
+  };
+  // ini cara menggunakan axios interceptorsnya
+  const axiosJWT = axios.create();
+  // akhir cara menggunakan axios interceptorsnya
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+        const response = await axios.get('http://localhost:5000/token');
+        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setToken(response.data.accessToken);
+        const decode = jwt_decoode(response.data.accessToken);
+        setName(decode.name);
+        setExpire(decode.exp);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const getUsers = async () => {
+    const response = await axiosJWT.get('http://localhost:5000/users', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data);
   };
   return (
     <div className="container mt-5">
       <h1>Welcome Back: {name}</h1>
+      <button onClick={getUsers} className="button is-info">
+        Get users
+      </button>
     </div>
   );
 };
